ef19f722cb07de3538a0ce20760d5e20f242ad2e
ForgeThePath2025 <forgethepath.2025@hotmail.com>
2025-12-17 22:57:06 +0000

    Implement parallel matrix multiplication using OpenMP for performance improvement

---
+++ test/cmdlineTests/~deduplicator-verbatim-bug/MatrixMultiplication.cpp
@@ -0,0 +1,54 @@
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <omp.h>
+
+void initializeMatrix(std::vector<std::vector<double>>& matrix, int size) {
+    matrix.resize(size, std::vector<double>(size));
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            matrix[i][j] = static_cast<double>(rand()) / RAND_MAX;
+        }
+    }
+}
+
+void multiplyMatrices(const std::vector<std::vector<double>>& A,
+                      const std::vector<std::vector<double>>& B,
+                      std::vector<std::vector<double>>& C,
+                      int size) {
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < size; ++i) {
+        for (int j = 0; j < size; ++j) {
+            double sum = 0.0;
+            for (int k = 0; k < size; ++k) {
+                sum += A[i][k] * B[k][j];
+            }
+            C[i][j] = sum;
+        }
+    }
+}
+
+int main() {
+    const int MATRIX_SIZE = 500;
+    std::vector<std::vector<double>> A, B, C;
+    
+    srand(static_cast<unsigned>(time(nullptr)));
+    
+    double start = omp_get_wtime();
+    initializeMatrix(A, MATRIX_SIZE);
+    initializeMatrix(B, MATRIX_SIZE);
+    C.resize(MATRIX_SIZE, std::vector<double>(MATRIX_SIZE, 0.0));
+    
+    multiplyMatrices(A, B, C, MATRIX_SIZE);
+    
+    double end = omp_get_wtime();
+    
+    std::cout << "Matrix multiplication completed for size " 
+              << MATRIX_SIZE << "x" << MATRIX_SIZE << std::endl;
+    std::cout << "Execution time: " << (end - start) << " seconds" << std::endl;
+    
+    return 0;
+}
