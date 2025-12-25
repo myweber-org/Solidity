04b58c2f4179cf93f956ca7e89099e753a7a1e95
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-25 20:37:34 +0000

    Implement matrix multiplication using nested loops for two 3x3 matrices

---
+++ test/cmdlineTests/yul_string_format_ascii/MatrixMultiplier.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> multiplyMatrices(const std::vector<std::vector<int>>& matA, const std::vector<std::vector<int>>& matB) {
+    int rowsA = matA.size();
+    int colsA = matA[0].size();
+    int colsB = matB[0].size();
+
+    std::vector<std::vector<int>> result(rowsA, std::vector<int>(colsB, 0));
+
+    for (int i = 0; i < rowsA; ++i) {
+        for (int j = 0; j < colsB; ++j) {
+            for (int k = 0; k < colsA; ++k) {
+                result[i][j] += matA[i][k] * matB[k][j];
+            }
+        }
+    }
+    return result;
+}
+
+void printMatrix(const std::vector<std::vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main() {
+    std::vector<std::vector<int>> matrixA = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    std::vector<std::vector<int>> matrixB = {
+        {9, 8, 7},
+        {6, 5, 4},
+        {3, 2, 1}
+    };
+
+    std::vector<std::vector<int>> product = multiplyMatrices(matrixA, matrixB);
+
+    std::cout << "Matrix A:" << std::endl;
+    printMatrix(matrixA);
+
+    std::cout << "\nMatrix B:" << std::endl;
+    printMatrix(matrixB);
+
+    std::cout << "\nProduct of A and B:" << std::endl;
+    printMatrix(product);
+
+    return 0;
+}
