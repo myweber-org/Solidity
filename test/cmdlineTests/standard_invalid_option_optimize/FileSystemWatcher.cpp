7dcc9787fa5742ee7689d94fa1f3390757c885e4
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2025-12-31 20:10:53 +0000

    Implement a cross-platform file system watcher using C++17 and std::filesystem

--- test/cmdlineTests/standard_invalid_option_optimize/FileSystemWatcher.cpp
+++ test/cmdlineTests/standard_invalid_option_optimize/FileSystemWatcher.cpp
@@ -111,4 +111,111 @@ int main() {
     }
 
     return 0;
+}#include <iostream>
+#include <filesystem>
+#include <chrono>
+#include <thread>
+#include <unordered_map>
+#include <functional>
+#include <atomic>
+#include <mutex>
+
+namespace fs = std::filesystem;
+
+class FileSystemWatcher {
+public:
+    using Callback = std::function<void(const fs::path&, const std::string&)>;
+
+    FileSystemWatcher() : running_(false) {}
+
+    void addWatchPath(const fs::path& path, Callback callback) {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if (fs::exists(path) && fs::is_directory(path)) {
+            watch_paths_[path] = {callback, fs::last_write_time(path)};
+        }
+    }
+
+    void start() {
+        running_ = true;
+        monitor_thread_ = std::thread(&FileSystemWatcher::monitor, this);
+    }
+
+    void stop() {
+        running_ = false;
+        if (monitor_thread_.joinable()) {
+            monitor_thread_.join();
+        }
+    }
+
+    ~FileSystemWatcher() {
+        stop();
+    }
+
+private:
+    void monitor() {
+        while (running_) {
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::lock_guard<std::mutex> lock(mutex_);
+
+            for (auto& [path, data] : watch_paths_) {
+                if (!fs::exists(path)) continue;
+
+                auto current_time = fs::last_write_time(path);
+                if (data.last_write_time != current_time) {
+                    data.callback(path, "modified");
+                    data.last_write_time = current_time;
+                }
+
+                for (const auto& entry : fs::recursive_directory_iterator(path)) {
+                    const auto& entry_path = entry.path();
+                    if (!tracked_files_.count(entry_path)) {
+                        if (fs::is_regular_file(entry_path)) {
+                            data.callback(entry_path, "created");
+                            tracked_files_.insert(entry_path);
+                        }
+                    }
+                }
+
+                auto it = tracked_files_.begin();
+                while (it != tracked_files_.end()) {
+                    if (!fs::exists(*it)) {
+                        data.callback(*it, "deleted");
+                        it = tracked_files_.erase(it);
+                    } else {
+                        ++it;
+                    }
+                }
+            }
+        }
+    }
+
+    struct WatchData {
+        Callback callback;
+        fs::file_time_type last_write_time;
+    };
+
+    std::unordered_map<fs::path, WatchData> watch_paths_;
+    std::unordered_set<fs::path> tracked_files_;
+    std::atomic<bool> running_;
+    std::thread monitor_thread_;
+    std::mutex mutex_;
+};
+
+int main() {
+    FileSystemWatcher watcher;
+
+    watcher.addWatchPath("./logs", [](const fs::path& path, const std::string& action) {
+        std::cout << "File " << path << " has been " << action << std::endl;
+    });
+
+    watcher.addWatchPath("./config", [](const fs::path& path, const std::string& action) {
+        std::cerr << "Config change detected: " << path << " -> " << action << std::endl;
+    });
+
+    watcher.start();
+
+    std::this_thread::sleep_for(std::chrono::seconds(30));
+    watcher.stop();
+
+    return 0;
 }
