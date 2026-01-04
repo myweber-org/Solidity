10d9cfd23f53f8be1e476ea2f3e5dcaf94984a64
ChromaticCatalyst <chromaticcatalyst@hotmail.com>
2026-01-04 20:16:50 +0000

    Implement parallel matrix multiplication using OpenMP for performance optimization

---
+++ test/libsolidity/syntaxTests/variableDeclaration/MatrixMultiplier.cpp
@@ -0,0 +1,85 @@
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <omp.h>
+
+using namespace std;
+
+vector<vector<double>> generateRandomMatrix(int rows, int cols) {
+    vector<vector<double>> matrix(rows, vector<double>(cols));
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            matrix[i][j] = static_cast<double>(rand()) / RAND_MAX;
+        }
+    }
+    return matrix;
+}
+
+vector<vector<double>> multiplyMatricesParallel(const vector<vector<double>>& A,
+                                                const vector<vector<double>>& B) {
+    int m = A.size();
+    int n = A[0].size();
+    int p = B[0].size();
+    
+    if (n != B.size()) {
+        cerr << "Matrix dimensions incompatible for multiplication." << endl;
+        exit(1);
+    }
+    
+    vector<vector<double>> result(m, vector<double>(p, 0.0));
+    
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < p; ++j) {
+            double sum = 0.0;
+            for (int k = 0; k < n; ++k) {
+                sum += A[i][k] * B[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+    
+    return result;
+}
+
+void printMatrixStats(const vector<vector<double>>& matrix, const string& name) {
+    double sum = 0.0;
+    double minVal = matrix[0][0];
+    double maxVal = matrix[0][0];
+    
+    for (const auto& row : matrix) {
+        for (double val : row) {
+            sum += val;
+            if (val < minVal) minVal = val;
+            if (val > maxVal) maxVal = val;
+        }
+    }
+    
+    cout << name << " - Size: " << matrix.size() << "x" << matrix[0].size() << endl;
+    cout << "Sum: " << sum << ", Min: " << minVal << ", Max: " << maxVal << endl;
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+    
+    const int SIZE = 500;
+    
+    cout << "Generating random matrices of size " << SIZE << "x" << SIZE << "..." << endl;
+    auto matrixA = generateRandomMatrix(SIZE, SIZE);
+    auto matrixB = generateRandomMatrix(SIZE, SIZE);
+    
+    cout << "Performing parallel matrix multiplication..." << endl;
+    double startTime = omp_get_wtime();
+    auto result = multiplyMatricesParallel(matrixA, matrixB);
+    double endTime = omp_get_wtime();
+    
+    cout << "Multiplication completed in " << (endTime - startTime) << " seconds." << endl;
+    
+    printMatrixStats(matrixA, "Matrix A");
+    printMatrixStats(matrixB, "Matrix B");
+    printMatrixStats(result, "Result Matrix");
+    
+    return 0;
+}
