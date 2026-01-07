9a9807c4931378f1a82b750f109ca26b52f4d8ee
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-01-07 14:54:37 +0000

    Implement parallel matrix multiplication using OpenMP for performance improvement

---
+++ test/cmdlineTests/model_checker_show_unproved_true_chc/MatrixMultiplication.cpp
@@ -0,0 +1,87 @@
+
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <omp.h>
+
+std::vector<std::vector<double>> generateRandomMatrix(int rows, int cols) {
+    std::vector<std::vector<double>> matrix(rows, std::vector<double>(cols));
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            matrix[i][j] = static_cast<double>(rand()) / RAND_MAX;
+        }
+    }
+    return matrix;
+}
+
+std::vector<std::vector<double>> multiplyMatrices(
+    const std::vector<std::vector<double>>& A,
+    const std::vector<std::vector<double>>& B) {
+    
+    int rowsA = A.size();
+    int colsA = A[0].size();
+    int colsB = B[0].size();
+    
+    std::vector<std::vector<double>> result(rowsA, std::vector<double>(colsB, 0.0));
+    
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < rowsA; ++i) {
+        for (int j = 0; j < colsB; ++j) {
+            double sum = 0.0;
+            for (int k = 0; k < colsA; ++k) {
+                sum += A[i][k] * B[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+    
+    return result;
+}
+
+void printMatrixStats(const std::vector<std::vector<double>>& matrix) {
+    double sum = 0.0;
+    double minVal = matrix[0][0];
+    double maxVal = matrix[0][0];
+    
+    #pragma omp parallel for reduction(+:sum) reduction(min:minVal) reduction(max:maxVal) collapse(2)
+    for (size_t i = 0; i < matrix.size(); ++i) {
+        for (size_t j = 0; j < matrix[i].size(); ++j) {
+            double val = matrix[i][j];
+            sum += val;
+            if (val < minVal) minVal = val;
+            if (val > maxVal) maxVal = val;
+        }
+    }
+    
+    std::cout << "Matrix statistics:\n";
+    std::cout << "  Sum: " << sum << "\n";
+    std::cout << "  Average: " << sum / (matrix.size() * matrix[0].size()) << "\n";
+    std::cout << "  Min: " << minVal << "\n";
+    std::cout << "  Max: " << maxVal << "\n";
+}
+
+int main() {
+    srand(static_cast<unsigned>(time(nullptr)));
+    
+    const int N = 500;
+    const int M = 500;
+    const int P = 500;
+    
+    std::cout << "Generating matrices of size " << N << "x" << M << " and " << M << "x" << P << "...\n";
+    
+    auto matrixA = generateRandomMatrix(N, M);
+    auto matrixB = generateRandomMatrix(M, P);
+    
+    std::cout << "Performing matrix multiplication with OpenMP...\n";
+    double startTime = omp_get_wtime();
+    
+    auto result = multiplyMatrices(matrixA, matrixB);
+    
+    double endTime = omp_get_wtime();
+    std::cout << "Multiplication completed in " << (endTime - startTime) << " seconds\n";
+    
+    printMatrixStats(result);
+    
+    return 0;
+}
