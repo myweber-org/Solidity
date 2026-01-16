be634734914f64562f5f1a3ffffaa28716ec0116
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-16 21:09:12 +0000

    Implement matrix multiplication using nested loops for 3x3 matrices.

--- test/libsolidity/syntaxTests/structs/MatrixMultiplication.cpp
+++ test/libsolidity/syntaxTests/structs/MatrixMultiplication.cpp
@@ -1,51 +1,50 @@
-
 #include <iostream>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <omp.h>
 
-void initializeMatrix(std::vector<std::vector<double>>& matrix, int size) {
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            matrix[i][j] = static_cast<double>(rand()) / RAND_MAX;
+std::vector<std::vector<int>> multiplyMatrices(const std::vector<std::vector<int>>& matA,
+                                               const std::vector<std::vector<int>>& matB) {
+    int rowsA = matA.size();
+    int colsA = matA[0].size();
+    int colsB = matB[0].size();
+
+    std::vector<std::vector<int>> result(rowsA, std::vector<int>(colsB, 0));
+
+    for (int i = 0; i < rowsA; ++i) {
+        for (int j = 0; j < colsB; ++j) {
+            for (int k = 0; k < colsA; ++k) {
+                result[i][j] += matA[i][k] * matB[k][j];
+            }
         }
     }
+    return result;
 }
 
-void multiplyMatrices(const std::vector<std::vector<double>>& A,
-                      const std::vector<std::vector<double>>& B,
-                      std::vector<std::vector<double>>& C,
-                      int size) {
-    #pragma omp parallel for collapse(2)
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            double sum = 0.0;
-            for (int k = 0; k < size; ++k) {
-                sum += A[i][k] * B[k][j];
-            }
-            C[i][j] = sum;
+void printMatrix(const std::vector<std::vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            std::cout << val << " ";
         }
+        std::cout << std::endl;
     }
 }
 
 int main() {
-    const int MATRIX_SIZE = 500;
-    srand(static_cast<unsigned>(time(nullptr)));
+    std::vector<std::vector<int>> matrixA = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
 
-    std::vector<std::vector<double>> A(MATRIX_SIZE, std::vector<double>(MATRIX_SIZE));
-    std::vector<std::vector<double>> B(MATRIX_SIZE, std::vector<double>(MATRIX_SIZE));
-    std::vector<std::vector<double>> C(MATRIX_SIZE, std::vector<double>(MATRIX_SIZE));
+    std::vector<std::vector<int>> matrixB = {
+        {9, 8, 7},
+        {6, 5, 4},
+        {3, 2, 1}
+    };
 
-    initializeMatrix(A, MATRIX_SIZE);
-    initializeMatrix(B, MATRIX_SIZE);
+    std::vector<std::vector<int>> product = multiplyMatrices(matrixA, matrixB);
 
-    double start_time = omp_get_wtime();
-    multiplyMatrices(A, B, C, MATRIX_SIZE);
-    double end_time = omp_get_wtime();
-
-    std::cout << "Matrix multiplication completed for size " << MATRIX_SIZE << std::endl;
-    std::cout << "Execution time: " << (end_time - start_time) << " seconds" << std::endl;
+    std::cout << "Result of matrix multiplication:" << std::endl;
+    printMatrix(product);
 
     return 0;
 }
