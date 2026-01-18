f46f70700d7c33e9496f9d1b9b6552c316b732b5
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-18 16:51:55 +0000

    Implement parallel matrix multiplication using OpenMP for performance optimization

--- test/libsolidity/semanticTests/strings/MatrixMultiplication.cpp
+++ test/libsolidity/semanticTests/strings/MatrixMultiplication.cpp
@@ -5,57 +5,56 @@
 #include <ctime>
 #include <omp.h>
 
-void initializeMatrix(std::vector<std::vector<double>>& matrix, int size) {
-    matrix.resize(size, std::vector<double>(size));
-    #pragma omp parallel for collapse(2)
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            matrix[i][j] = static_cast<double>(rand()) / RAND_MAX;
-        }
-    }
-}
-
-void multiplyMatrices(const std::vector<std::vector<double>>& A,
-                      const std::vector<std::vector<double>>& B,
-                      std::vector<std::vector<double>>& C,
-                      int size) {
-    C.resize(size, std::vector<double>(size, 0.0));
-    
-    #pragma omp parallel for collapse(2)
-    for (int i = 0; i < size; ++i) {
-        for (int j = 0; j < size; ++j) {
-            double sum = 0.0;
-            #pragma omp simd reduction(+:sum)
-            for (int k = 0; k < size; ++k) {
-                sum += A[i][k] * B[k][j];
+class ParallelMatrixMultiplier {
+public:
+    static std::vector<std::vector<double>> multiply(const std::vector<std::vector<double>>& A,
+                                                     const std::vector<std::vector<double>>& B) {
+        size_t rowsA = A.size();
+        size_t colsA = A[0].size();
+        size_t colsB = B[0].size();
+        
+        std::vector<std::vector<double>> result(rowsA, std::vector<double>(colsB, 0.0));
+        
+        #pragma omp parallel for collapse(2)
+        for (size_t i = 0; i < rowsA; ++i) {
+            for (size_t j = 0; j < colsB; ++j) {
+                double sum = 0.0;
+                for (size_t k = 0; k < colsA; ++k) {
+                    sum += A[i][k] * B[k][j];
+                }
+                result[i][j] = sum;
             }
-            C[i][j] = sum;
+        }
+        
+        return result;
+    }
+    
+    static void initializeRandomMatrix(std::vector<std::vector<double>>& matrix) {
+        #pragma omp parallel for
+        for (size_t i = 0; i < matrix.size(); ++i) {
+            for (size_t j = 0; j < matrix[0].size(); ++j) {
+                matrix[i][j] = static_cast<double>(rand()) / RAND_MAX * 10.0;
+            }
         }
     }
-}
+};
 
 int main() {
-    const int MATRIX_SIZE = 512;
-    std::vector<std::vector<double>> A, B, C;
-    
+    const size_t N = 500;
     srand(static_cast<unsigned>(time(nullptr)));
     
-    double start = omp_get_wtime();
-    initializeMatrix(A, MATRIX_SIZE);
-    initializeMatrix(B, MATRIX_SIZE);
-    double init_time = omp_get_wtime() - start;
+    std::vector<std::vector<double>> matrixA(N, std::vector<double>(N));
+    std::vector<std::vector<double>> matrixB(N, std::vector<double>(N));
     
-    start = omp_get_wtime();
-    multiplyMatrices(A, B, C, MATRIX_SIZE);
-    double mult_time = omp_get_wtime() - start;
+    ParallelMatrixMultiplier::initializeRandomMatrix(matrixA);
+    ParallelMatrixMultiplier::initializeRandomMatrix(matrixB);
     
-    std::cout << "Matrix size: " << MATRIX_SIZE << "x" << MATRIX_SIZE << std::endl;
-    std::cout << "Initialization time: " << init_time << " seconds" << std::endl;
-    std::cout << "Multiplication time: " << mult_time << " seconds" << std::endl;
+    double startTime = omp_get_wtime();
+    auto result = ParallelMatrixMultiplier::multiply(matrixA, matrixB);
+    double endTime = omp_get_wtime();
     
-    double total_ops = 2.0 * MATRIX_SIZE * MATRIX_SIZE * MATRIX_SIZE;
-    double gflops = (total_ops / 1e9) / mult_time;
-    std::cout << "Performance: " << gflops << " GFLOPS" << std::endl;
+    std::cout << "Matrix multiplication completed for " << N << "x" << N << " matrices." << std::endl;
+    std::cout << "Execution time: " << (endTime - startTime) << " seconds" << std::endl;
     
     return 0;
 }
