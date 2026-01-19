06613ba3c1b46297cbac7682d3c1a2cd105413fb
OaklandAvenue <my.oakland.avenue@outlook.com>
2026-01-19 20:52:49 +0000

    Implement matrix multiplication using nested loops for 3x3 matrices

--- test/cmdlineTests/model_checker_divModSlacks_false_chc/MatrixMultiplier.cpp
+++ test/cmdlineTests/model_checker_divModSlacks_false_chc/MatrixMultiplier.cpp
@@ -1,60 +1,53 @@
-
 #include <iostream>
 #include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <omp.h>
 
-class ParallelMatrixMultiplier {
-public:
-    static std::vector<std::vector<double>> multiply(const std::vector<std::vector<double>>& A,
-                                                     const std::vector<std::vector<double>>& B) {
-        size_t rowsA = A.size();
-        size_t colsA = A[0].size();
-        size_t colsB = B[0].size();
-        
-        std::vector<std::vector<double>> result(rowsA, std::vector<double>(colsB, 0.0));
-        
-        #pragma omp parallel for collapse(2)
-        for (size_t i = 0; i < rowsA; ++i) {
-            for (size_t j = 0; j < colsB; ++j) {
-                double sum = 0.0;
-                for (size_t k = 0; k < colsA; ++k) {
-                    sum += A[i][k] * B[k][j];
-                }
-                result[i][j] = sum;
-            }
-        }
-        
-        return result;
-    }
-    
-    static void initializeRandomMatrix(std::vector<std::vector<double>>& matrix) {
-        #pragma omp parallel for
-        for (size_t i = 0; i < matrix.size(); ++i) {
-            for (size_t j = 0; j < matrix[0].size(); ++j) {
-                matrix[i][j] = static_cast<double>(rand()) / RAND_MAX * 100.0;
+std::vector<std::vector<int>> multiplyMatrices(const std::vector<std::vector<int>>& matA,
+                                               const std::vector<std::vector<int>>& matB) {
+    int rowsA = matA.size();
+    int colsA = matA[0].size();
+    int colsB = matB[0].size();
+
+    std::vector<std::vector<int>> result(rowsA, std::vector<int>(colsB, 0));
+
+    for (int i = 0; i < rowsA; ++i) {
+        for (int j = 0; j < colsB; ++j) {
+            for (int k = 0; k < colsA; ++k) {
+                result[i][j] += matA[i][k] * matB[k][j];
             }
         }
     }
-};
+
+    return result;
+}
+
+void printMatrix(const std::vector<std::vector<int>>& matrix) {
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            std::cout << val << " ";
+        }
+        std::cout << std::endl;
+    }
+}
 
 int main() {
-    const size_t N = 512;
-    srand(static_cast<unsigned>(time(nullptr)));
-    
-    std::vector<std::vector<double>> matrixA(N, std::vector<double>(N));
-    std::vector<std::vector<double>> matrixB(N, std::vector<double>(N));
-    
-    ParallelMatrixMultiplier::initializeRandomMatrix(matrixA);
-    ParallelMatrixMultiplier::initializeRandomMatrix(matrixB);
-    
-    double startTime = omp_get_wtime();
-    auto result = ParallelMatrixMultiplier::multiply(matrixA, matrixB);
-    double endTime = omp_get_wtime();
-    
-    std::cout << "Matrix multiplication completed for " << N << "x" << N << " matrices." << std::endl;
-    std::cout << "Execution time: " << (endTime - startTime) << " seconds" << std::endl;
-    
+    std::vector<std::vector<int>> matrixA = {{1, 2, 3},
+                                             {4, 5, 6},
+                                             {7, 8, 9}};
+
+    std::vector<std::vector<int>> matrixB = {{9, 8, 7},
+                                             {6, 5, 4},
+                                             {3, 2, 1}};
+
+    std::vector<std::vector<int>> product = multiplyMatrices(matrixA, matrixB);
+
+    std::cout << "Matrix A:" << std::endl;
+    printMatrix(matrixA);
+
+    std::cout << "\nMatrix B:" << std::endl;
+    printMatrix(matrixB);
+
+    std::cout << "\nProduct of A and B:" << std::endl;
+    printMatrix(product);
+
     return 0;
 }
