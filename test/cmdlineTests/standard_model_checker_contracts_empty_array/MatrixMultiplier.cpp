81d0c293814d8605b27f4851e9ff2af8e6457a4f
TheQuietQuotient <The.Quiet.Quotient@outlook.com>
2026-01-31 22:42:10 +0000

    Implement parallel matrix multiplication using OpenMP for performance optimization

--- test/cmdlineTests/standard_model_checker_contracts_empty_array/MatrixMultiplier.cpp
+++ test/cmdlineTests/standard_model_checker_contracts_empty_array/MatrixMultiplier.cpp
@@ -1,47 +1,87 @@
 
 #include <iostream>
 #include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <omp.h>
 
-std::vector<std::vector<int>> multiplyMatrices(const std::vector<std::vector<int>>& A,
-                                               const std::vector<std::vector<int>>& B) {
+using namespace std;
+
+vector<vector<double>> generateRandomMatrix(int rows, int cols) {
+    vector<vector<double>> matrix(rows, vector<double>(cols));
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            matrix[i][j] = static_cast<double>(rand()) / RAND_MAX;
+        }
+    }
+    return matrix;
+}
+
+vector<vector<double>> multiplyMatrices(const vector<vector<double>>& A,
+                                        const vector<vector<double>>& B) {
     int rowsA = A.size();
     int colsA = A[0].size();
     int colsB = B[0].size();
-
-    std::vector<std::vector<int>> result(rowsA, std::vector<int>(colsB, 0));
-
+    
+    vector<vector<double>> result(rowsA, vector<double>(colsB, 0.0));
+    
+    #pragma omp parallel for collapse(2)
     for (int i = 0; i < rowsA; ++i) {
         for (int j = 0; j < colsB; ++j) {
+            double sum = 0.0;
             for (int k = 0; k < colsA; ++k) {
-                result[i][j] += A[i][k] * B[k][j];
+                sum += A[i][k] * B[k][j];
             }
+            result[i][j] = sum;
         }
     }
-
+    
     return result;
 }
 
-void printMatrix(const std::vector<std::vector<int>>& matrix) {
-    for (const auto& row : matrix) {
-        for (int val : row) {
-            std::cout << val << " ";
+void printMatrixStats(const vector<vector<double>>& matrix) {
+    double sum = 0.0;
+    double minVal = matrix[0][0];
+    double maxVal = matrix[0][0];
+    
+    #pragma omp parallel for reduction(+:sum) reduction(min:minVal) reduction(max:maxVal) collapse(2)
+    for (size_t i = 0; i < matrix.size(); ++i) {
+        for (size_t j = 0; j < matrix[i].size(); ++j) {
+            double val = matrix[i][j];
+            sum += val;
+            if (val < minVal) minVal = val;
+            if (val > maxVal) maxVal = val;
         }
-        std::cout << std::endl;
     }
+    
+    cout << "Matrix statistics:" << endl;
+    cout << "  Sum: " << sum << endl;
+    cout << "  Average: " << sum / (matrix.size() * matrix[0].size()) << endl;
+    cout << "  Min value: " << minVal << endl;
+    cout << "  Max value: " << maxVal << endl;
 }
 
 int main() {
-    std::vector<std::vector<int>> matrixA = {{1, 2, 3},
-                                             {4, 5, 6}};
-
-    std::vector<std::vector<int>> matrixB = {{7, 8},
-                                             {9, 10},
-                                             {11, 12}};
-
-    std::vector<std::vector<int>> product = multiplyMatrices(matrixA, matrixB);
-
-    std::cout << "Result of matrix multiplication:" << std::endl;
-    printMatrix(product);
-
+    srand(static_cast<unsigned>(time(nullptr)));
+    
+    const int SIZE = 500;
+    
+    cout << "Generating random matrices of size " << SIZE << "x" << SIZE << "..." << endl;
+    auto matrixA = generateRandomMatrix(SIZE, SIZE);
+    auto matrixB = generateRandomMatrix(SIZE, SIZE);
+    
+    cout << "Performing matrix multiplication with OpenMP parallelization..." << endl;
+    double startTime = omp_get_wtime();
+    
+    auto result = multiplyMatrices(matrixA, matrixB);
+    
+    double endTime = omp_get_wtime();
+    double elapsedTime = endTime - startTime;
+    
+    cout << "Multiplication completed in " << elapsedTime << " seconds" << endl;
+    cout << "Performance: " << (2.0 * SIZE * SIZE * SIZE) / (elapsedTime * 1e9) << " GFLOPS" << endl;
+    
+    printMatrixStats(result);
+    
     return 0;
 }
