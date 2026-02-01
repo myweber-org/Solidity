208d908ce971ec7022a55aa6826fbc3a26e900f8
TheCobaltProtocol <The.Cobalt.Protocol@hotmail.com>
2026-02-01 00:23:16 +0000

    Implement parallel matrix multiplication using OpenMP for performance optimization

--- test/cmdlineTests/model_checker_show_proved_safe_true_chc/MatrixMultiplier.cpp
+++ test/cmdlineTests/model_checker_show_proved_safe_true_chc/MatrixMultiplier.cpp
@@ -30,19 +30,19 @@ public:
     }
     
     static void initializeRandomMatrix(std::vector<std::vector<double>>& matrix) {
-        #pragma omp parallel for collapse(2)
+        #pragma omp parallel for
         for (size_t i = 0; i < matrix.size(); ++i) {
             for (size_t j = 0; j < matrix[0].size(); ++j) {
-                matrix[i][j] = static_cast<double>(rand()) / RAND_MAX * 100.0;
+                matrix[i][j] = static_cast<double>(rand()) / RAND_MAX;
             }
         }
     }
 };
 
 int main() {
+    const size_t N = 512;
     srand(static_cast<unsigned>(time(nullptr)));
     
-    const size_t N = 512;
     std::vector<std::vector<double>> A(N, std::vector<double>(N));
     std::vector<std::vector<double>> B(N, std::vector<double>(N));
     
@@ -53,7 +53,7 @@ int main() {
     auto result = ParallelMatrixMultiplier::multiply(A, B);
     double end_time = omp_get_wtime();
     
-    std::cout << "Matrix multiplication completed for " << N << "x" << N << " matrices." << std::endl;
+    std::cout << "Matrix multiplication completed for " << N << "x" << N << " matrices" << std::endl;
     std::cout << "Execution time: " << (end_time - start_time) << " seconds" << std::endl;
     
     return 0;
