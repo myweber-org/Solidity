59afae58958eb3193aaf18cbf59e93ea9209eb5c
PixelPioneerStudio521 <Pixel.Pioneer.Studio@outlook.com>
2026-02-05 02:21:37 +0000

    Implement parallel matrix multiplication using OpenMP for performance optimization

--- test/libsolidity/syntaxTests/dataLocations/privateFunction/MatrixMultiplier.cpp
+++ test/libsolidity/syntaxTests/dataLocations/privateFunction/MatrixMultiplier.cpp
@@ -5,83 +5,104 @@
 #include <ctime>
 #include <omp.h>
 
-using namespace std;
+class ParallelMatrixMultiplier {
+private:
+    std::vector<std::vector<double>> matrixA;
+    std::vector<std::vector<double>> matrixB;
+    std::vector<std::vector<double>> result;
+    size_t rowsA, colsA, rowsB, colsB;
 
-vector<vector<double>> generateRandomMatrix(int rows, int cols) {
-    vector<vector<double>> matrix(rows, vector<double>(cols));
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            matrix[i][j] = static_cast<double>(rand()) / RAND_MAX;
+public:
+    ParallelMatrixMultiplier(size_t rA, size_t cA, size_t rB, size_t cB) 
+        : rowsA(rA), colsA(cA), rowsB(rB), colsB(cB) {
+        
+        if (colsA != rowsB) {
+            throw std::invalid_argument("Matrix dimensions incompatible for multiplication");
         }
-    }
-    return matrix;
-}
 
-vector<vector<double>> multiplyMatricesParallel(const vector<vector<double>>& A,
-                                                const vector<vector<double>>& B) {
-    int rowsA = A.size();
-    int colsA = A[0].size();
-    int colsB = B[0].size();
-    
-    vector<vector<double>> result(rowsA, vector<double>(colsB, 0.0));
-    
-    #pragma omp parallel for collapse(2)
-    for (int i = 0; i < rowsA; ++i) {
-        for (int j = 0; j < colsB; ++j) {
-            double sum = 0.0;
-            for (int k = 0; k < colsA; ++k) {
-                sum += A[i][k] * B[k][j];
+        matrixA.resize(rowsA, std::vector<double>(colsA));
+        matrixB.resize(rowsB, std::vector<double>(colsB));
+        result.resize(rowsA, std::vector<double>(colsB, 0.0));
+
+        initializeMatrices();
+    }
+
+    void initializeMatrices() {
+        std::srand(static_cast<unsigned>(std::time(nullptr)));
+        
+        #pragma omp parallel for collapse(2)
+        for (size_t i = 0; i < rowsA; ++i) {
+            for (size_t j = 0; j < colsA; ++j) {
+                matrixA[i][j] = static_cast<double>(std::rand()) / RAND_MAX;
             }
-            result[i][j] = sum;
         }
-    }
-    
-    return result;
-}
 
-void printMatrixStats(const vector<vector<double>>& matrix, const string& name) {
-    double sum = 0.0;
-    double minVal = matrix[0][0];
-    double maxVal = matrix[0][0];
-    
-    for (const auto& row : matrix) {
-        for (double val : row) {
-            sum += val;
-            if (val < minVal) minVal = val;
-            if (val > maxVal) maxVal = val;
+        #pragma omp parallel for collapse(2)
+        for (size_t i = 0; i < rowsB; ++i) {
+            for (size_t j = 0; j < colsB; ++j) {
+                matrixB[i][j] = static_cast<double>(std::rand()) / RAND_MAX;
+            }
         }
     }
-    
-    cout << name << " - Size: " << matrix.size() << "x" << matrix[0].size()
-         << ", Sum: " << sum
-         << ", Min: " << minVal
-         << ", Max: " << maxVal << endl;
-}
+
+    void multiply() {
+        #pragma omp parallel for collapse(2)
+        for (size_t i = 0; i < rowsA; ++i) {
+            for (size_t j = 0; j < colsB; ++j) {
+                double sum = 0.0;
+                #pragma omp simd reduction(+:sum)
+                for (size_t k = 0; k < colsA; ++k) {
+                    sum += matrixA[i][k] * matrixB[k][j];
+                }
+                result[i][j] = sum;
+            }
+        }
+    }
+
+    void displayResult(size_t maxRows = 5, size_t maxCols = 5) const {
+        std::cout << "Result matrix (first " << maxRows << "x" << maxCols << " elements):\n";
+        for (size_t i = 0; i < std::min(rowsA, maxRows); ++i) {
+            for (size_t j = 0; j < std::min(colsB, maxCols); ++j) {
+                std::cout << result[i][j] << " ";
+            }
+            std::cout << "\n";
+        }
+    }
+
+    double verifyMultiplication() const {
+        double totalSum = 0.0;
+        #pragma omp parallel for reduction(+:totalSum) collapse(2)
+        for (size_t i = 0; i < rowsA; ++i) {
+            for (size_t j = 0; j < colsB; ++j) {
+                totalSum += result[i][j];
+            }
+        }
+        return totalSum;
+    }
+};
 
 int main() {
-    srand(static_cast<unsigned>(time(nullptr)));
+    const size_t SIZE = 512;
     
-    const int SIZE = 500;
-    
-    cout << "Generating random matrices of size " << SIZE << "x" << SIZE << "..." << endl;
-    auto matrixA = generateRandomMatrix(SIZE, SIZE);
-    auto matrixB = generateRandomMatrix(SIZE, SIZE);
-    
-    printMatrixStats(matrixA, "Matrix A");
-    printMatrixStats(matrixB, "Matrix B");
-    
-    cout << "\nPerforming parallel matrix multiplication..." << endl;
-    double startTime = omp_get_wtime();
-    
-    auto result = multiplyMatricesParallel(matrixA, matrixB);
-    
-    double endTime = omp_get_wtime();
-    double elapsedTime = endTime - startTime;
-    
-    printMatrixStats(result, "Result Matrix");
-    cout << "\nComputation time: " << elapsedTime << " seconds" << endl;
-    cout << "Performance: " << (2.0 * SIZE * SIZE * SIZE) / (elapsedTime * 1e9)
-         << " GFLOPS" << endl;
+    try {
+        ParallelMatrixMultiplier multiplier(SIZE, SIZE, SIZE, SIZE);
+        
+        double startTime = omp_get_wtime();
+        multiplier.multiply();
+        double endTime = omp_get_wtime();
+        
+        std::cout << "Matrix multiplication completed for " << SIZE << "x" << SIZE << " matrices\n";
+        std::cout << "Execution time: " << (endTime - startTime) << " seconds\n";
+        
+        multiplier.displayResult();
+        
+        double checksum = multiplier.verifyMultiplication();
+        std::cout << "Matrix checksum: " << checksum << "\n";
+        
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
+    }
     
     return 0;
 }
